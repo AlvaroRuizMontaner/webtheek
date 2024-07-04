4d370cbb573a896f28174f3f03fc2c859b46e3ea
Varojausz <zoezil_ifra@hotmail.com>
2024-07-04 12:31:11 +0200

    commit

--- app/projects/create/page.tsx
+++ app/projects/create/page.tsx
@@ -1,6 +1,6 @@
 "use client"
-import CreateProjectView from "@/views/projects/CreateProjectView";
 
+import CreateProjectView from "@/views/projects/CreateProjectView";
 
 export default function Page(): JSX.Element {
 
