b2f61d28ed83a82b6a092790ad8aeeb14c9bac6c
Varojausz <zoezil_ifra@hotmail.com>
2024-07-04 11:37:13 +0200

    import route createProjectView

--- app/projects/create/page.tsx
+++ app/projects/create/page.tsx
@@ -1,5 +1,5 @@
 "use client"
-import CreateProjectView from "@/views/projects/CreateProjectView";
+import CreateProjectView from "../../../views/projects/CreateProjectView";
 
 
 export default function Page(): JSX.Element {
