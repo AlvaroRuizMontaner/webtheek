499137d18db1a8a174da08a40719b9f3cd5a4f86
Varojausz <zoezil_ifra@hotmail.com>
2024-07-22 16:48:34 +0200

    link fonts

--- utils/generateHtml.ts
+++ utils/generateHtml.ts
@@ -24,6 +24,8 @@ export const getHtmlWithStyles = (htmlElement: MutableRefObject<HTMLDivElement |
                     <meta http-equiv="X-UA-Compatible" content="IE=edge">
                     <meta name="viewport" content="width=device-width, initial-scale=1">
                     <style>
+                   <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100..900;1,100..900&family=Rubik:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&family=Rubik:ital,wght@0,300..900;1,300..900&display=swap" rel="stylesheet">
+
                         ${styles}
                     </style>
                 </head>
