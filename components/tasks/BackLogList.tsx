7267ab49280b596df021938dcdfea6100b99484f
Varojausz <zoezil_ifra@hotmail.com>
2024-08-09 14:30:14 +0200

    mejoras backlog front

--- components/tasks/BackLogList.tsx
+++ components/tasks/BackLogList.tsx
@@ -1,12 +1,5 @@
 import { Project, TaskProject } from '@/types'
-import React, { Fragment } from 'react'
-import "./task.css"
-import { useRouter } from 'next/navigation'
-import { Menu, Transition } from '@headlessui/react'
-import { EllipsisVerticalIcon } from '@heroicons/react/20/solid'
-import { useMutation, useQueryClient } from '@tanstack/react-query'
-import { deleteTask } from '@/services/TaskAPI'
-import { toast } from 'react-toastify'
+import BacklogCard from './BacklogCard'
 
 type BackLogListProps = {
     backlogGroupedTasks: TaskProject[]
@@ -16,97 +9,20 @@ type BackLogListProps = {
 
 export default function BackLogList({backlogGroupedTasks, canEdit, projectId}: BackLogListProps) {
 
-    const router = useRouter()
-
-    const queryClient = useQueryClient()
-    const { mutate } = useMutation({
-      mutationFn: deleteTask,
-      onError: (error) => {
-        toast.error(error.message)
-      },
-      onSuccess: (data) => {
-        toast.success(data)
-        queryClient.invalidateQueries({queryKey: ["project", projectId]})
-      },
-    })
-
-  return (
-    <>
-    <div className='mt-10 font-black headline3'>
-        <p className=''>Backlog</p>
-    </div>
-    <ul className='my-6 space-y-2'>
-        {backlogGroupedTasks.map((task, index) => (
-            <li key={task.name + index} className="p-2 border-sm backlog-card rounded-lg flex justify-between">
-                <div className='font-bold text-gray-600 flex flex-col justify-center'>
-                    <span className='cursor-pointer' 
-                        onClick={() =>
-                            router.push(location.pathname + `?viewTask=${task._id}`)
-                        }
-                    >
-                        {task.name}
-                    </span>
-                </div>
-
-                <div className="flex shrink-0  gap-x-6">
-                    <Menu as="div" className="relative flex-none">
-                    <Menu.Button className="-m-2.5 block p-2.5 text-gray-500 hover:text-gray-900">
-                        <span className="sr-only">opciones</span>
-                        <EllipsisVerticalIcon className="h-9 w-9" aria-hidden="true" />
-                    </Menu.Button>
-                    <Transition
-                        as={Fragment}
-                        enter="transition ease-out duration-100"
-                        enterFrom="transform opacity-0 scale-95"
-                        enterTo="transform opacity-100 scale-100"
-                        leave="transition ease-in duration-75"
-                        leaveFrom="transform opacity-100 scale-100"
-                        leaveTo="transform opacity-0 scale-95"
-                    >
-                        <Menu.Items className="absolute right-0 z-10 mt-2 w-56 origin-top-right rounded-md bg-white py-2 shadow-lg ring-1 ring-gray-900/5 focus:outline-none">
-                        <Menu.Item>
-                            <button
-                            type="button"
-                            className="block px-3 py-1 text-sm leading-6 text-gray-900"
-                            onClick={() =>
-                                router.push(location.pathname + `?viewTask=${task._id}`)
-                            }
-                            >
-                            Ver Tarea
-                            </button>
-                        </Menu.Item>
-                        {canEdit && (
-                            <>
-                            <Menu.Item>
-                                <button
-                                type="button"
-                                className="block px-3 py-1 text-sm leading-6 text-gray-900"
-                                onClick={() =>
-                                    router.push(location.pathname + `?editTask=${task._id}`)
-                                }
-                                >
-                                Editar Tarea
-                                </button>
-                            </Menu.Item>
-
-                            <Menu.Item>
-                                <button
-                                type="button"
-                                className="block px-3 py-1 text-sm leading-6 text-red-500"
-                                onClick={() => mutate({ projectId, taskId: task._id })}
-                                >
-                                Eliminar Tarea
-                                </button>
-                            </Menu.Item>
-                            </>
-                        )}
-                        </Menu.Items>
-                    </Transition>
-                    </Menu>
-                </div>
-            </li>
-        ))}
-    </ul>
-    </>
-  )
-}
+    return (
+        <>
+          <div className='mt-10 font-black headline3'>
+            <p>Backlog</p>
+          </div>
+          {backlogGroupedTasks.length !== 0 ? (
+            <ul className='my-6 space-y-2'>
+              {backlogGroupedTasks.map((task, index) => (
+                <BacklogCard key={task.name + index} task={task} canEdit={canEdit} projectId={projectId} />
+              ))}
+            </ul>
+          ) : (
+            <div className='my-6 body3'>No hay tareas en el backlog</div>
+          )}
+        </>
+      );
+    }   
