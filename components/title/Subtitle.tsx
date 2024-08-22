530e7d530d5d4ba3c0838fd653e7405ea6897973
Varojausz <zoezil_ifra@hotmail.com>
2024-08-22 20:00:25 +0200

    componentes input en profile

--- components/title/Subtitle.tsx
+++ components/title/Subtitle.tsx
@@ -4,15 +4,16 @@ type SubtitleProps = {
     highlight: string
     text: string
     children?: ReactNode
+    variant?: string
 }
 
-export default function Subtitle({highlight, text, children}: SubtitleProps) {
+export default function Subtitle({highlight, text, children, variant="light"}: SubtitleProps) {
   return (
     <>
       {!children ? (
-        <p className="body1 font-light text-white my-6u">
+        <p className={`body1 font-light my-6u ${variant === "light" ? "text-white" : "text-gray-700"}`}>
           {text} {""}
-          <span className=" text-accent-300 font-bold">{highlight}</span>
+          <span className={`font-bold ${variant === "light" ? "text-accent-300" : "text-accent-500"}`}>{highlight}</span>
         </p>
       ) : (
         children
