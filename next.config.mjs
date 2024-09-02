2fd922bfa3c5d17088f4105ab0afbac88a50d6db
Varojausz <zoezil_ifra@hotmail.com>
2024-09-02 13:29:17 +0200

    fixes after tasklist, icons and button primary

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    // Esto permite importar svgs como componentes en Next que luego pueden colorearse (importante poner fill: currentColor en el svg de origen)
     webpack(config) {
       config.module.rules.push({
         test: /\.svg$/,
