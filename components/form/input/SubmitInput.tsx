71a99b968835012d46fcc07b8e43b9c99dd90457
Varojausz <zoezil_ifra@hotmail.com>
2024-09-05 15:38:07 +0200

    commit

--- components/form/input/SubmitInput.tsx
+++ components/form/input/SubmitInput.tsx
@@ -8,11 +8,11 @@ type SubmitInputProps = {
 
 export default function SubmitInput({isLoading, value}: SubmitInputProps) {
   return (
-    <div className="bg-accent-500 hover:bg-accent-700 w-full flex justify-center h-[52px] text-white text-xl cursor-pointer relative rounded-md transition-color duration-300">
+    <div className="bg-accent-500 hover:bg-accent-700 w-full justify-center h-[52px] flex text-white text-xl cursor-pointer relative rounded-md transition-color duration-300">
         {!isLoading ? <input
         type="submit"
         value={value}
-        className="block w-full h-full p-3 cursor-pointer"
+        className="w-full h-full p-3 cursor-pointer block"
         /> : <Spinner />}
     </div>
   )
