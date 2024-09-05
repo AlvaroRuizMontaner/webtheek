7d9fd649a817896fb1921ebc302913df14fd9302
Varojausz <zoezil_ifra@hotmail.com>
2024-09-06 00:33:04 +0200

    establecer animaciones generales, poner enlace directo a login al deslogear, colocar animaciones, hacer el link del Button a tamaño completo

--- components/button/Button.tsx
+++ components/button/Button.tsx
@@ -18,9 +18,10 @@ type ButtonProps = Omit<Props, "className">
 
 const LinkButton = ({text, className, href}: LinkButtonProps): JSX.Element => {
     return (
-      <nav className={className}>
+      <nav>
         <Link
           href={href!}
+          className={className}
         >
           {text}
         </Link>
@@ -42,7 +43,7 @@ const ActionButton = ({text, className, onClick}: ActionButtonProps): JSX.Elemen
 export default function Button({href, text, variant="normal", onClick}: ButtonProps) {
 
     
-    let className = "text-xl px-8u py-2u min-w-full sm:min-w-fit sm:max-w-fit rounded-lg bg-primary-400 hover:bg-primary-700 border-2 border-primary-500 text-white cursor-pointer flex justify-center items-center transition-colors duration-300"
+    let className = "__press w-full h-full text-xl px-8u py-2u min-w-full sm:min-w-fit sm:max-w-fit rounded-lg bg-primary-400 hover:bg-primary-700 border-2 border-primary-500 text-white cursor-pointer flex justify-center items-center transition-colors duration-300"
     if(variant === "outline") className += " " + "bg-white hover:!bg-primary-100 border-2 border-primary-500 !text-primary-500 font-bold shadow-inset"
 
   return (
--- components/loading-templates/ProjectsLoading.tsx
+++ components/loading-templates/ProjectsLoading.tsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import "./loading-templates.css"
 
 const ProjectsLoadingData = [
     {
@@ -36,15 +35,15 @@ export default function ProjectsLoading() {
   return (
     <div>
       <div className=" h-[159px] sm:h-[174px] lg:h-[189px] mb-12u flex flex-col justify-between">
-        <section className='loading-text h-[36px] sm:h-[48px] lg:h-[60px] max-w-[300px]'></section>       
-        <section className='loading-text h-4 max-w-[316px]'></section>
-        <section className='loading-text h-[48px] max-w-full sm:max-w-[220px]'></section>
+        <section className='__loading h-[36px] sm:h-[48px] lg:h-[60px] max-w-[300px]'></section>       
+        <section className='__loading h-4 max-w-[316px]'></section>
+        <section className='__loading rounded-lg h-[48px] max-w-full sm:max-w-[220px]'></section>
       </div>
       <ul role="list" className="space-y-4 my-10">
         {ProjectsLoadingData.map((project, index) => (
           <li
             key={project._id + index}
-            className="flex relative justify-between gap-x-6 bg-white shadow-lg loading-bar h-[191px] sm:h-[198px] lg:h-[203px]"
+            className="flex relative justify-between gap-x-6 shadow-lg __loading h-[191px] sm:h-[198px] lg:h-[203px]"
           >
             <div className="flex min-w-0 gap-x-4 pl-6u py-8u overflow-hidden">
               <div className="min-w-0 flex-auto space-y-2u">
