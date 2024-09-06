428975e3094b9fcadc112463f620402633801e91
Varojausz <zoezil_ifra@hotmail.com>
2024-09-06 13:24:28 +0200

    projectTeamView parcial

--- views/projects/ProjectTeamView.tsx
+++ views/projects/ProjectTeamView.tsx
@@ -1,5 +1,9 @@
+import Button from '@/components/button/Button'
 import ProjectsLoading from '@/components/loading-templates/ProjectsLoading'
+import PermissionTag from '@/components/permission-tag/PermissionTag'
 import AddMemberModal from '@/components/team/AddMemberModal'
+import Subtitle from '@/components/title/Subtitle'
+import Title from '@/components/title/Title'
 import { getProjectTeam, removeUserFromProject } from '@/services/TeamAPI'
 import { Project } from '@/types'
 import { Menu, MenuButton, MenuItem, MenuItems, Transition } from '@headlessui/react'
@@ -39,92 +43,112 @@ export default function ProjectTeamView({projectId}: ProjectTeamViewProps) {
   if (isLoading) return <ProjectsLoading />;
   if (isError) throw new Error("Error");
   if (data) return (
-      <>
-        <h1 className=" headline2 font-black">Administrar equipo</h1>
-        <p className="fon-light text-gray-500 mt-5 body1">
-          Administra el equipo de este proyecto
-        </p>
+    <>
+      <Title variant="dark">Administrar equipo</Title>
+      <Subtitle variant="dark" text="Administra el equipo de este proyecto" />
 
-        <nav className="my-5 flex flex-col sm:flex-row gap-3">
-          <button
-            type="button"
-            className=" bg-primary-400 hover:bg-primary-700 px-10 py-3 text-white body2
-                font-bold cursor-pointer transition-colors text-center"
-            onClick={() => router.push("?addMember=true")}
-          >
-            Agregar Colaborador
-          </button>
+      <nav className="flex flex-col gap-3 sm:flex-row">
+        <Button
+          text="Agregar Colaborador"
+          onClick={() => router.push("?addMember=true")}
+        />
+        <Button
+          href={"/projects/" + projectId}
+          text="Volver a proyecto"
+          variant="outline"
+        />
+      </nav>
 
-          <Link
-            href={"/projects/" + projectId}
-            className=" bg-white hover:bg-primary-100 border-2 border-primary-500 px-10 py-3 text-primary-500 body2
-                font-bold cursor-pointer transition-colors text-center"
-          >
-            Volver a proyecto
-          </Link>
-        </nav>
+      <h2 className="headline2 font-black mt-16u mb-8u sm:mt-24u sm:mb-12u">
+        Miembros actuales
+      </h2>
+      {data.length ? (
+        <ul
+          role="list"
+          className="space-y-4 my-10"
+        >
+          {data?.map((member) => (
+            <li
+              key={member.user._id}
+              className="flex relative justify-between gap-x-6 project-card bg-white shadow-lg"
+            >
+              <div className="flex min-w-0 gap-x-4 pl-6u py-8u overflow-hidden">
+                <div className="min-w-0 flex-auto space-y-2u">
+                  <div className="permissionTagWrapper">
+                    <PermissionTag
+                      isManager={false}
+                      permissionLevel={member.permissionLevel}
+                    />
+                  </div>
+                  <div>
+                    <Link
+                      href={location.pathname + `/${member.user._id}`}
+                      className="text-gray-600 cursor-pointer hover:underline headline3 font-bold"
+                    >
+                      {member.user.name}
+                    </Link>
+                  </div>
+                  <p className="text-sm text-gray-400">
+                    {member.user.email}
+                  </p>
+                </div>
+              </div>
 
-        <h2 className="headline2 font-black my-10">Miembros actuales</h2>
-            {data.length ? (
-                <ul role="list" className="divide-y divide-gray-100 border border-gray-100 my-10 bg-white shadow-lg">
-                    {data?.map((member) => (
-                        <li key={member.user._id} className="flex justify-between gap-x-6 px-5 py-10">
-                            <div className="flex min-w-0 gap-x-4">
-                                <div className="min-w-0 flex-auto space-y-2">
-                                    <p className="text-2xl font-black text-gray-600">
-                                        <Link href={location.pathname + `/${member.user._id}`}>{member.user.name}</Link>
-                                    </p>
-                                    <p className="text-sm text-gray-400">
-                                        {member.user.email}
-                                    </p>
-                                </div>
-                            </div>
-                            <div className="flex shrink-0 items-center gap-x-6">
-                                <Menu as="div" className="relative flex-none">
-                                    <MenuButton className="-m-2.5 block p-2.5 text-gray-500 hover:text-gray-900">
-                                            <span className="sr-only">opciones</span>
-                                            <EllipsisVerticalIcon className="h-9 w-9" aria-hidden="true" />
-                                    </MenuButton>
-                                    <Transition
-                                        as={Fragment}
-                                        enter="transition ease-out duration-100"
-                                        enterFrom="transform opacity-0 scale-95"
-                                        enterTo="transform opacity-100 scale-100"
-                                        leave="transition ease-in duration-75"
-                                        leaveFrom="transform opacity-100 scale-100"
-                                        leaveTo="transform opacity-0 scale-95"
-                                    >
-                                        <MenuItems className="absolute right-0 z-10 mt-2 w-56 origin-top-right rounded-md bg-white py-2 shadow-lg ring-1 ring-gray-900/5 focus:outline-none">
-                                            <MenuItem>
-                                                <button
-                                                    type='button'
-                                                    className='block px-3 py-1 text-sm leading-6 text-gray-900'
-                                                    onClick={() => router.push(location.pathname + `/${member.user._id}`)}
-                                                >
-                                                    Ver Colaborador
-                                                </button>
-                                            </MenuItem>
-                                            <MenuItem>
-                                                <button
-                                                    type='button'
-                                                    className='block px-3 py-1 text-sm leading-6 text-red-500'
-                                                    onClick={() => mutate({projectId, userId: member.user._id})}
-                                                >
-                                                    Eliminar del Proyecto
-                                                </button>
-                                            </MenuItem>
-                                        </MenuItems>
-                                    </Transition>
-                                </Menu>
-                            </div>
-                        </li>
-                    ))}
-                </ul>
-            ) : (
-                <p className='text-center py-20'>No hay miembros en este equipo</p>
-            )}
+              <div className="flex shrink-0 items-center gap-x-6 pr-6u py-8u">
+                <Menu as="div" className="relative flex-none">
+                  <MenuButton className="-m-2.5 block p-2.5 text-gray-500 hover:text-gray-900">
+                    <span className="sr-only">opciones</span>
+                    <EllipsisVerticalIcon
+                      className="h-9 w-9"
+                      aria-hidden="true"
+                    />
+                  </MenuButton>
+                  <Transition
+                    as={Fragment}
+                    enter="transition ease-out duration-100"
+                    enterFrom="transform opacity-0 scale-95"
+                    enterTo="transform opacity-100 scale-100"
+                    leave="transition ease-in duration-75"
+                    leaveFrom="transform opacity-100 scale-100"
+                    leaveTo="transform opacity-0 scale-95"
+                  >
+                    <MenuItems className="absolute right-0 z-10 mt-2 w-56 origin-top-right rounded-md bg-white py-2 shadow-lg ring-1 ring-gray-900/5 focus:outline-none">
+                      <MenuItem>
+                        <button
+                          type="button"
+                          className="block px-3 py-1 text-sm leading-6 text-gray-900"
+                          onClick={() =>
+                            router.push(
+                              location.pathname + `/${member.user._id}`
+                            )
+                          }
+                        >
+                          Ver Colaborador
+                        </button>
+                      </MenuItem>
+                      <MenuItem>
+                        <button
+                          type="button"
+                          className="block px-3 py-1 text-sm leading-6 text-red-500"
+                          onClick={() =>
+                            mutate({ projectId, userId: member.user._id })
+                          }
+                        >
+                          Eliminar del Proyecto
+                        </button>
+                      </MenuItem>
+                    </MenuItems>
+                  </Transition>
+                </Menu>
+              </div>
+            </li>
+          ))}
+        </ul>
+      ) : (
+        <p className="text-center py-20">No hay miembros en este equipo</p>
+      )}
 
-        <AddMemberModal projectId={projectId} />
-      </>
-    );
+      <AddMemberModal projectId={projectId} />
+    </>
+  );
 }
