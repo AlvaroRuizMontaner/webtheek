1ae53f9ac8fbfebde45f0fb5f106fff12ada47ff
Varojausz <zoezil_ifra@hotmail.com>
2024-09-19 22:36:06 +0200

    banner-home provisional 3

--- components/banner/BannerHome.tsx
+++ components/banner/BannerHome.tsx
@@ -4,21 +4,31 @@ import Button from '../button/Button'
 
 export default function BannerHome() {
   return (
-    <section className="banner-home w-full h-[600px]">
-      <div className="container grid md:grid-cols-2 h-full">
-        <article className="w-fit h-full flex flex-col justify-end md:justify-center">
-          <div className='content-banner p-6u sm:p-8u rounded-lg space-y-6u'>
-            <ul className='space-y-6u list-disc pl-5 text-lg'>
-                <li className="font-bold">Planea y crea proyectos con tu equipo</li>
-                <li className="font-bold">Colaborando se llegan a mejores resultados</li>
-            </ul>
-            <div className='flex justify-center'>
-            <Button text="Ir a proyectos" href="/projects" variant='outline' />
+    <div className="contenedor-banner-home">
+      <section className="banner-home w-full h-full">
+        <div className="container grid md:grid-cols-2 h-full">
+          <article className="w-fit h-full flex flex-col justify-end md:justify-center">
+            <div className="content-banner p-6u sm:p-8u rounded-lg space-y-6u">
+              <ul className="space-y-6u list-disc pl-5 text-lg">
+                <li className="font-bold">
+                  Planea y crea proyectos con tu equipo
+                </li>
+                <li className="font-bold">
+                  Controla y dirige a tus colaboradores
+                </li>
+              </ul>
+              <div className="flex justify-center">
+                <Button
+                  text="Ir a proyectos"
+                  href="/projects"
+                  variant="outline"
+                />
+              </div>
             </div>
-          </div>
-        </article>
-        <article></article>
-      </div>
-    </section>
+          </article>
+          <article></article>
+        </div>
+      </section>
+    </div>
   );
 }
