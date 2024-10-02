7c92b2af637c0eccd3d5bb6913d59256a7f9b7f2
Varojausz <zoezil_ifra@hotmail.com>
2024-10-02 17:56:36 +0200

    styled scrollbar, refactor backlog

--- components/tasks/BackLogList.tsx
+++ components/tasks/BackLogList.tsx
@@ -1,5 +1,7 @@
 import { Project, TaskProject } from '@/types'
 import BacklogCard from './BacklogCard'
+import Title from '../title/Title'
+import Subtitle from '../title/Subtitle'
 
 type BackLogListProps = {
     backlogGroupedTasks: TaskProject[]
@@ -10,19 +12,36 @@ type BackLogListProps = {
 export default function BackLogList({backlogGroupedTasks, canEdit, projectId}: BackLogListProps) {
 
     return (
-        <>
-          <div className='mt-16u mb-8u sm:mt-24u sm:mb-12u font-black headline3'>
-            <p>Backlog</p>
-          </div>
-          {backlogGroupedTasks.length !== 0 ? (
-            <ul className='my-6 space-y-2'>
-              {backlogGroupedTasks.map((task, index) => (
-                <BacklogCard key={task.name + index} task={task} canEdit={canEdit} projectId={projectId} />
-              ))}
-            </ul>
-          ) : (
-            <div className='my-6 body3'>No hay tareas en el backlog</div>
-          )}
-        </>
-      );
+      <>
+        <div className="mt-16u mb-8u sm:mt-24u sm:mb-12u">
+          <Title as="h2" variant="dark">
+            Backlog
+          </Title>
+          <Subtitle variant="dark" text="Buzón de tareas" />
+{/*           <nav className="flex flex-col gap-3 sm:flex-row">
+            {(isManager(data.manager, user._id) ||
+              havePermission(data.team, user._id, 3)) && (
+              <Button
+                text="Agregar Tarea"
+                onClick={() => router.push("?newTask=true")}
+              />
+            )}
+          </nav> */}
+        </div>
+        {backlogGroupedTasks.length !== 0 ? (
+          <ul className="my-6 space-y-2">
+            {backlogGroupedTasks.map((task, index) => (
+              <BacklogCard
+                key={task.name + index}
+                task={task}
+                canEdit={canEdit}
+                projectId={projectId}
+              />
+            ))}
+          </ul>
+        ) : (
+          <div className="my-6 body3">No hay tareas en el backlog</div>
+        )}
+      </>
+    );
     }   
