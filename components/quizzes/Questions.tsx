461da67ea41e8b8dd237c460f6ae233d2e050f82
Varojausz <zoezil_ifra@hotmail.com>
2024-10-08 14:52:23 +0200

    insertar questions parcial 3

--- components/quizzes/Questions.tsx
+++ components/quizzes/Questions.tsx
@@ -2,18 +2,34 @@ import Question from './Question';
 import { Quiz } from '@/types/quiz';
 
 type QuestionsProps = {
-    questions: Quiz["questions"]
+    dataQuestions: Quiz["questions"]
+    stateQuestions: any
 }
 
-export default function Questions({questions}: QuestionsProps) {
+export default function Questions({dataQuestions, stateQuestions}: QuestionsProps) {
+
+  function calculateIndexOfStateQuestion(dataQuestions, stateQuestionIndex) {
+    return dataQuestions.length + stateQuestionIndex
+  }
+
   return (
-    <section className="border bg-primary-200  w-full rounded-md p-6">
-      <div className="bg-gray-100 h-full space-y-8u rounded-md">
-        {questions.map((question, questionIndex) => (
+    <section className="border bg-primary-200 w-full rounded-t-md p-6">
+      <div className="bg-gray-100 space-y-8u first:rounded-t-md">
+        {dataQuestions.map((question, questionIndex) => (
           <Question
             key={"questionName" + questionIndex}
             question={question}
             questionIndex={questionIndex}
+          />
+        ))}
+      </div>
+
+      <div className="bg-gray-100 space-y-8u last:rounded-b-md pt-8u">
+        {stateQuestions.map((question, stateQuestionIndex) => (
+          <Question
+            key={"questionName" + calculateIndexOfStateQuestion(dataQuestions, stateQuestionIndex)}
+            question={question}
+            questionIndex={calculateIndexOfStateQuestion(dataQuestions, stateQuestionIndex)}
           />
         ))}
       </div>
