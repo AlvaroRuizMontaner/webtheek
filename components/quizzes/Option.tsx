b1959a56d8116c3f0bc5f288d526ec2e334ef1e0
Varojausz <zoezil_ifra@hotmail.com>
2024-10-09 12:59:19 +0200

    [feat] questions listo

--- components/quizzes/Option.tsx
+++ components/quizzes/Option.tsx
@@ -2,20 +2,22 @@ import { OptionType } from '@/types/quiz';
 
 type OptionProps = {
     option: OptionType
-    questionIndex: number
+    /* questionIndex: number */
+    optionIndex: number
+    correctIndex: string
 }
 
-export default function Option({option, questionIndex}:OptionProps) {
+export default function Option({option, /* questionIndex, */ optionIndex, correctIndex}:OptionProps) {
   return (
     <div
-      className="flex gap-2 items-center min-h-[42px] bg-gray-100"
+      className={`flex gap-2 items-center min-h-[42px]  ${optionIndex.toString() === correctIndex ? "bg-accent-100" : "bg-gray-100"}`}
     >
-      <input
+{/*       <input
         type="radio"
         name={"question" + questionIndex}
         id={"question" + questionIndex}
-      />
-      <div>{option.text}</div>
+      /> */}
+      <div className={`px-2`}>{option.text}</div>
     </div>
   );
 }
