0b375b559acd78798db996ec8ea5594cd8e46f31
Varojausz <zoezil_ifra@hotmail.com>
2024-11-25 16:04:30 +0100

    commit any 2

--- components/curriculum-templates/CuerpoLateral.tsx
+++ components/curriculum-templates/CuerpoLateral.tsx
@@ -1,8 +1,10 @@
 import React from 'react'
 import SeccionCuerpoLateral from './SeccionCuerpoLateral'
+import { seccionCuerpoLateralInfoType } from './templates.info'
 
 
-export default function CuerpoLateral({page}: {page: any[]}) {
+
+export default function CuerpoLateral({page}: {page: seccionCuerpoLateralInfoType[]}) {
   return (
     <div className='flex flex-col justify-center gap-5 p-[1.25rem] text-white'>
       {page.map((seccion, seccionIndx) => (
