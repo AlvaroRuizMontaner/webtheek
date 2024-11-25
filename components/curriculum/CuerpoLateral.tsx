07c82d93c0e3550140c11cd7953c6015cca144cd
Varojausz <zoezil_ifra@hotmail.com>
2024-11-25 15:51:14 +0100

    commit

--- components/curriculum/CuerpoLateral.tsx
+++ components/curriculum/CuerpoLateral.tsx
@@ -3,7 +3,7 @@ import SeccionCuerpoLateral from './SeccionCuerpoLateral'
 import { seccionCuerpoLateralInfoType } from './curriculum.info'
 
 
-export default function CuerpoLateral({page}: {page: seccionCuerpoLateralInfoType[]}) {
+export default function CuerpoLateral({page}:{page: seccionCuerpoLateralInfoType[]}) {
   return (
     <div className='flex flex-col justify-center gap-5 p-[1.25rem] text-white'>
       {page.map((seccion, seccionIndx) => (
