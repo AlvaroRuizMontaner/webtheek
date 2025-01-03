46a9ee99f637b77c6032de77b18eecf813fd44b1
Varojausz <zoezil_ifra@hotmail.com>
2025-01-03 22:47:04 +0100

    lint

--- services/CurriculumAPI.ts
+++ services/CurriculumAPI.ts
@@ -1,58 +1,5 @@
 import api from "@/lib/axios";
-import { Curriculum, CurriculumFormData } from "@/types/curriculum";
 import { AxiosError } from "axios";
-
-type CurriculumApiType = {
-    endpointName: "createCurriculum" | "getCurriculums" | "getCurriculumById";
-    formData?: CurriculumFormData,
-    curriculumId?: Curriculum["_id"]
-}
-
-
-export const createCurriculum = async (formData: CurriculumApiType["formData"]) => {
-    try {
-      const response = await api.post(`/curriculums`, formData)
-      return { data: response.data };
-    } catch (error) {
-      const axiosError = error as AxiosError;
-      return {
-        error: {
-          status: axiosError.response?.status || 500,
-          data: axiosError.response?.data || axiosError.message,
-        },
-      };
-    }
-  };
-
-export const getCurriculums = async () => {
-  try {
-    const response = await api.get("/curriculums")
-    return { data: response.data };
-  } catch (error) {
-    const axiosError = error as AxiosError;
-    return {
-      error: {
-        status: axiosError.response?.status || 500,
-        data: axiosError.response?.data || axiosError.message,
-      },
-    };
-  }
-};
-
-export const getCurriculumById = async (curriculumId: CurriculumApiType["curriculumId"]) => {
-  try {
-    const response = await api.get(`/curriculums/${curriculumId}`)
-    return { data: response.data };
-  } catch (error) {
-    const axiosError = error as AxiosError;
-    return {
-      error: {
-        status: axiosError.response?.status || 500,
-        data: axiosError.response?.data || axiosError.message,
-      },
-    };
-  }
-};
 
 // Base Query
 
