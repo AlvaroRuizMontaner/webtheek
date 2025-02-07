a13092eb5bc9992ec9350729fb583a4a5b681240
Varojausz <zoezil_ifra@hotmail.com>
2025-02-07 17:09:56 +0100

    NEXT_PUBLIC_API_URL_CYPRESS

--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -1,7 +1,7 @@
 Cypress.Commands.add('login', (email, password) => {
     cy.request({
       method: 'POST',
-      url: `${process.env.NEXT_PUBLIC_API_URL}/auth/login`,
+      url: `${process.env.NEXT_PUBLIC_API_URL_CYPRESS}/auth/login`,
       body: { email, password },
     }).then((response) => {
       expect(response.status).to.eq(200); // Verifica que el login fue exitoso
