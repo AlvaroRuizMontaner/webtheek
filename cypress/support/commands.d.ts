52764ef1a8e5fba59c6fbdff3308a316778fe9de
Varojausz <zoezil_ifra@hotmail.com>
2025-02-10 18:15:08 +0100

    ensayo CYPRESS logs 13

---
+++ cypress/support/commands.d.ts
@@ -0,0 +1,22 @@
+export {}
+
+declare global {
+    namespace Cypress {
+      interface Chainable {
+
+        //Custom command to select DOM element by data-cy attribute.
+        login(email: string, password: string): Chainable<void>
+      }
+    }
+}
+
+/* 
+// support/index.d.ts
+/// <reference types="cypress" />
+
+declare namespace Cypress {
+  interface Chainable {
+    login(email: string, password: string): void;
+  }
+}
+*/
--- cypress/support/index.d.ts
+++
@@ -1,8 +0,0 @@
-// support/index.d.ts
-/// <reference types="cypress" />
-
-declare namespace Cypress {
-  interface Chainable {
-    login(email: string, password: string): void;
-  }
-}
