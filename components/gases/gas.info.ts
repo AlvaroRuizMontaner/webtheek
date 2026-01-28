805cf389d3258e411a3a2c59fb1a3465dfb48357
Varojausz <zoezil_ifra@hotmail.com>
2026-01-28 19:00:46 +0100

    rebuild para key stripe

---
+++ components/gases/gas.info.ts
@@ -0,0 +1,25 @@
+export type Gas = {
+    name: string;
+    formula: string;
+    molarFraction: number;
+}
+
+/* export type SideInfoChildType = {
+    icon: {
+        nameIcon: string;
+        classNameIcon: string;
+    };
+    main: string;
+    aux: string
+    bar?: string
+    mainType: number
+}
+
+export type SectionCentralBodyInfoType = {
+    title: {
+        text: string;
+        nameIcon: string;
+        classNameIcon: string;
+    }
+    info: InfoChildType[]
+} */
